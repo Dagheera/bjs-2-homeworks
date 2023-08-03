15fe00e85927bd5286cbb6413ac0cd010b98f9d6
Darina Baltaeva <darina-baltaeva@yandex.ru>
2023-08-03 19:30:57 +0300

    Четвертое задание выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.hasOwnProperty('marks') === true) {
+        this.marks.push(...marks);
+    }    
 }
 
 Student.prototype.getAverage = function () {
-  
+   if (this.hasOwnProperty('marks') === false || this.marks.length === 0) {
+       return 0;
+   }
+    let sum = 0;
+    sum = this.marks.reduce((currentSum, currentVal) => currentSum + currentVal, 0);
+    return +(sum / this.marks.length).toFixed(2);  
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    this.excluded = reason;
+    delete this.marks;
+    delete this.subject;
+
 }
